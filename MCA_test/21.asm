19ff1f9fa266f08ac367aeb8883002e580521ef4
usernotfound0 <prohacking321@gmail.com>
2019-04-21 22:13:13 +0530

    MCA
    corrected 21.asm file

--- MCA_test/21.asm
+++ MCA_test/21.asm
@@ -5,7 +5,18 @@ mov a,@r0
 inc r0
 subb a,@r0
 da a
+jc down
 inc r0
 mov @r0,a
-stop: sjmp stop
+here: sjmp here
+down:cpl a
+inc a
+da a
+inc r0
+mov @r0,a
+inc r0
+clr a
+addc a,#00h
+mov @r0,a
+here1: sjmp here1
 end
