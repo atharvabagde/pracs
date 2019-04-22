5e4d6ac7c01d4025de0080d7d1d1faec3d6b569c
usernotfound0 <49810980+usernotfound0@users.noreply.github.com>
2019-04-22 09:42:20 +0530

    MCA
    Updated 21.asm file

--- MCA_test/21.asm
+++ MCA_test/21.asm
@@ -1,22 +1,31 @@
 ;21.	WAP to subtract 2-digit BCD numbers. (Verified)
 
-mov r0,#10h
+org 0000h
+mov r0,#21H
 mov a,@r0
-inc r0
-subb a,@r0
+mov r1,a
+dec r0
+mov a,@r0
+subb a,r1
 da a
 jc down
 inc r0
+inc r0
 mov @r0,a
 here: sjmp here
-down:cpl a
-inc a
-da a
+
+down: inc r0
+mov a,@r0
+mov r2,a
+mov a,#99h
+subb a,r2
+add a,#1h
+add a,#1h
+dec r0
+add a,@r0
+da a 
 inc r0
-mov @r0,a
 inc r0
-clr a
-addc a,#00h
 mov @r0,a
 here1: sjmp here1
 end
